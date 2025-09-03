b65761b76cb19cf01df89fb54acb00b0f593c1bc
aaronwai <aaron.lung.wai@gmail.com>
2025-09-03 15:05:09 +0800

    Step 1 : init the basic config

---
+++ config/settings/local.py
@@ -0,0 +1,28 @@
+
+from os import getenv,path
+
+from dotenv import load_dotenv
+
+from .base import * # noqa
+
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, '.envs', 'env.local')
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+    
+DEBUG = True   
+SITE_NAME= getenv('SITE_NAME')
+SECRET_KEY = getenv("DJANGO_SECRET_KEY", "2FqhCBRPtSQzslgwXE5QG_Opl-tNooV8DiiJFbUzO24oLo9-rhI") 
+'django-insecure-*7j$3b_h*%8$*8fbuqm9(vmnxirk=g_@ub6of+z#oc@9-rtn%g'
+
+
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ADMIN_URL = getenv("DJANGO_ADMIN_URL")
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
