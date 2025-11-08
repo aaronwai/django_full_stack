db297ff9fff192cd9bb19415ef27e96491bd98dd
aaronwai <aaron.lung.wai@gmail.com>
2025-11-09 04:31:50 +0800

    step 18 : testing jwt and minor bug fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_PAGINATION_CLASS": (
-        "rest_framework.pagenation.PageNumberPagination",
+        "rest_framework.pagination.PageNumberPagination",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend",],
     "PAGE_SIZE": 10,
