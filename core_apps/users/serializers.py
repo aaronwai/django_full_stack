cf26bc5d7007341ec6156fd5e435922d621dc0fc
aaronwai <aaron.lung.wai@gmail.com>
2025-11-09 14:27:32 +0800

    step 20 : djoser serializer

--- core_apps/users/serializers.py
+++ core_apps/users/serializers.py
@@ -1,9 +1,26 @@
 from django.contrib.auth import get_user_model
-from djoser.serializers  import UserCreateSerializer
-
+from djoser.serializers  import UserCreateSerializer, UserSerializer
+from django_countries.serializer_fields import CountryField
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
 User = get_user_model()
 
 class CreateUserSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'password']
+
+class CustomUserSerializer(UserSerializer):
+    full_name = serializers.ReadOnlyField(source='get_full_name')  
+    gender = serializers.ReadOnlyField(source='profile.gender') 
+    slug = serializers.ReadOnlyField(source='profile.slug')
+    occupation = serializers.ReadOnlyField(source='profile.occupation')
+    city = serializers.ReadOnlyField(source='profile.city_of_origin')
+    country = CountryField(source='profile.country_of_origin')
+    phone_number = PhoneNumberField(source='profile.phone_number')
+    avatar = serializers.ReadOnlyField(source='profile.avatar.url')
+    reputation = serializers.ReadOnlyField(source='profile.reputation')
+    class Meta(UserSerializer.Meta):
+        model = User
+        fields = ['id', "email",'first_name', 'last_name', 'slug','username',  'full_name', 'gender',  'occupation','phone_number', 'city', 'country',  'avatar', 'reputation', "date_joined"]
+        read_only_fields = ['id','email', 'date_joined']
