e028076b9725c53a55d3822a3c5d6ad43d6cb5d5
aaronwai <aaron.lung.wai@gmail.com>
2025-11-10 21:41:53 +0800

    step 29 : global fonts selected

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -26,6 +26,10 @@ const config: Config = {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
 			},
+			fontFamily: {
+				openSans: ["var(--font-openSans)"],
+				robotoSlab: ["var(--font-robotoSlab)"],
+			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
