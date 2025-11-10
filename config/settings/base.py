b908ffde58ada536d71db32dd2f38fa27a00807d
aaronwai <aaron.lung.wai@gmail.com>
2025-11-10 13:52:20 +0800

    step 24 : issues crud and minor bug fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -161,9 +161,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
-    "DEFAULT_PAGINATION_CLASS": (
+    "DEFAULT_PAGINATION_CLASS": 
         "rest_framework.pagination.PageNumberPagination",
-    ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend",],
     "PAGE_SIZE": 10,
     "DEFAULT_THROTTLE_CLASSES": (
