be6b89699bb6d9e4b2e8afe4e7891bc2ac5e5bb4
aaronwai <aaron.lung.wai@gmail.com>
2025-11-16 18:39:41 +0800

    step 43 : password reset

--- client/src/lib/redux/features/api/baseApiSlice.ts
+++ client/src/lib/redux/features/api/baseApiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApiSlice = createApi({
 	reducerPath: "api",
-	baseQuery: fetchBaseQuery({ baseUrl: "api/v1/", credentials: "include" }),
+	baseQuery: fetchBaseQuery({ baseUrl: "/api/v1/", credentials: "include" }),
 
 	endpoints: (builder) => ({}),
 });
