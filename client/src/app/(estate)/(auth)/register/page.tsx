b007caffe1ea85b3343b3aafe1eaedf822c2ffe3
aaronwai <aaron.lung.wai@gmail.com>
2025-11-17 12:51:29 +0800

    step 46 : google login

--- client/src/app/(estate)/(auth)/register/page.tsx
+++ client/src/app/(estate)/(auth)/register/page.tsx
@@ -1,6 +1,10 @@
+"use client";
 import { AuthFormHeader, RegisterForm } from "@/components/forms/auth";
+import OauthButtons from "@/components/shared/OauthButtons";
+import { useRedirectIfAuthenticated } from "@/hooks";
 
 export default function RegisterPage() {
+	useRedirectIfAuthenticated();
 	return (
 		<div>
 			<AuthFormHeader
@@ -13,6 +17,12 @@ export default function RegisterPage() {
 				<div className="bg-lightGrey dark:bg-deepBlueGrey rounded-xl px-6 py-12 shadow sm:rounded-lg sm:px-12 md:rounded-3xl">
 					<RegisterForm />
 				</div>
+				<div className="flex-center mt-5 space-x-2">
+					<div className="bg-richBlack dark:bg-platinum h-px flex-1"></div>
+					<span className="dark:text-platinum px-2 text-sm">Or</span>
+					<div className="bg-richBlack dark:bg-platinum h-px flex-1"></div>
+				</div>
+				<OauthButtons />
 			</div>
 		</div>
 	);
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,2 +1,4 @@
+import InitiateSocialAuth from "./initiateSocialAuth";
 export { default as extractErrorMessage } from "./extractErrorMessage";
 export { default as PersistAuth } from "./PersistAuth";
+export const UseGoogle = () => InitiateSocialAuth("google-oauth2", "google");
