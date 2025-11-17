b95d132b94d02459bc801bfe412eab532826c468
aaronwai <aaron.lung.wai@gmail.com>
2025-11-17 20:51:36 +0800

    step 50 : add tenants card

--- client/src/app/(estate)/tenants/page.tsx
+++ client/src/app/(estate)/tenants/page.tsx
@@ -1,32 +1,20 @@
-"use client";
-import { useGetAllUsersQuery } from "@/lib/redux/features/users/usersApiSlice";
-import Spinner from "@/components/shared/Spinner";
+import TenantCard from "@/components/cards/TenantCard";
 import ProtectedRoute from "@/components/shared/ProtectedRoutes";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+	title: "Homeplace Apartments | Tenants",
+	description:
+		"Authenticated users can view basic information about other tenants within the property. Tenants can also search for other tenants",
+};
 function TenantsPageContent() {
-	const { data, isLoading } = useGetAllUsersQuery({});
-	if (isLoading) {
-		return (
-			<div className="flex-center pt-32">
-				<Spinner size="xl" />
-			</div>
-		);
-	}
 	return (
 		<div>
-			<h1 className="dark:text-pumpkin text-6xl">Tenants</h1>
-			{data && data.profiles.results.length > 0 ? (
-				data.profiles.results.map((tenant: any) => (
-					<p key={tenant.id} className="dark:text-pumpkin text-4xl">
-						{tenant.full_name} - {tenant.occupation}
-					</p>
-				))
-			) : (
-				<p>No tenants found</p>
-			)}
+			<TenantCard />
 		</div>
 	);
 }
+
 export default function TenantsPage() {
 	return (
 		<ProtectedRoute>
