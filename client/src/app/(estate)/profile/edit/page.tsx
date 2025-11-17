1aff5ca22b1c6b3361d2fef251bd5ff583965f70
aaronwai <aaron.lung.wai@gmail.com>
2025-11-17 18:58:33 +0800

    step 49 : edit profile form

--- client/src/app/(estate)/profile/edit/page.tsx
+++ client/src/app/(estate)/profile/edit/page.tsx
@@ -1,12 +1,18 @@
 import { AuthFormHeader } from "@/components/forms/auth";
+import EditProfileForm from "@/components/forms/profile/EditProfileForm";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+	title: "Homeplace Apartments | Profile Edit",
+	description: "Signed in users can edit their profile information",
+};
 export default function EditProfilePage() {
 	return (
 		<div>
 			<AuthFormHeader title="Update Profile" />
 			<div className="mt-7 sm:mx-auto sm:w-full sm:max-w-[480px]">
 				<div className="bg-lightGrey dark:bg-deepBlueGrey rounded-xl px-6 py-12 shadow sm:rounded-lg sm:px-12 md:rounded-3xl">
-					<h1 className="dark:text-badyPowder text-6xl">Edit Profile</h1>
+					<EditProfileForm />
 				</div>
 			</div>
 		</div>
