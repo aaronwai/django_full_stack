99a547f7f381f38d185e345548683639789e9b95
aaronwai <aaron.lung.wai@gmail.com>
2025-11-18 12:50:44 +0800

    step 57 : report tenant

--- client/src/lib/redux/features/api/baseApiSlice.ts
+++ client/src/lib/redux/features/api/baseApiSlice.ts
@@ -54,7 +54,7 @@ const baseQueryWithReauth: BaseQueryFn<
 export const baseApiSlice = createApi({
 	reducerPath: "api",
 	baseQuery: baseQueryWithReauth,
-	tagTypes: ["User", "Apartment", "Issue"],
+	tagTypes: ["User", "Apartment", "Issue", "Report"],
 	refetchOnFocus: true,
 	refetchOnMountOrArgChange: true,
 	endpoints: (builder) => ({}),
