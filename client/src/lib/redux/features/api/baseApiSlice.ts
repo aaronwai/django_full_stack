8dd049261f992fcb5c516da1993323497ecb07e4
aaronwai <aaron.lung.wai@gmail.com>
2025-11-18 10:20:25 +0800

    step 53 : tenant add apartment

--- client/src/lib/redux/features/api/baseApiSlice.ts
+++ client/src/lib/redux/features/api/baseApiSlice.ts
@@ -54,7 +54,7 @@ const baseQueryWithReauth: BaseQueryFn<
 export const baseApiSlice = createApi({
 	reducerPath: "api",
 	baseQuery: baseQueryWithReauth,
-	tagTypes: ["User"],
+	tagTypes: ["User", "Apartment"],
 	refetchOnFocus: true,
 	refetchOnMountOrArgChange: true,
 	endpoints: (builder) => ({}),
