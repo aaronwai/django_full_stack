d4c77948846bd0dc74b4687decaf0493514daa19
aaronwai <aaron.lung.wai@gmail.com>
2025-11-18 14:37:39 +0800

    step 61 : refactor the pagination

--- client/src/components/cards/TenantCard.tsx
+++ client/src/components/cards/TenantCard.tsx
@@ -119,7 +119,7 @@ function TenantCardContent() {
 					<p>No tenants found</p>
 				)}
 			</div>
-			<PaginationSection totalPages={totalPages} />
+			<PaginationSection totalPages={totalPages} entityType="user" />
 		</div>
 	);
 }
